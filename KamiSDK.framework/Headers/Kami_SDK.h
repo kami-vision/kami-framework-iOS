05ab1ac07be045508ffddf41ded924fd6d87c5e4
Khetaram Kumawat <khetaram@kamivision.com>
2021-12-21 15:53:56 +0530

    Kami framework initial commit

---
+++ KamiSDK.framework/Headers/Kami_SDK.h
@@ -0,0 +1,18 @@
+//
+//  Kami_SDK.h
+//  Kami SDK
+//
+//  Created by Khetaram Kumawat on 30/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Kami_SDK.
+FOUNDATION_EXPORT double Kami_SDKVersionNumber;
+
+//! Project version string for Kami_SDK.
+FOUNDATION_EXPORT const unsigned char Kami_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kami_SDK/PublicHeader.h>
+
+
